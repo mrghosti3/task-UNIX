154ea7879a77815d281480d73c6cf1fe23ccc1e5
mrghosti3 <mysteriousghst@gmail.com>
2022-05-29 17:24:01 +0300

    Integrated tree.h

--- src/main.c
+++ src/main.c
@@ -10,11 +10,12 @@
 #include "libs/queue.h"
 #define XXH_INLINE_ALL
 #include "libs/xxhash.h"
-#include "libs/tree.h" // TODO: implement tree usage
+#include "libs/tree.h"
 
 // queue.h Integrations
 
 // NOTE: may be better to use student struct as entry on SLIST_ENTRY
+// FIX: implement double-hashing for hash collision resolution
 struct student {
     char *name;
     size_t nlen;
@@ -83,7 +84,17 @@ struct slist_stud* instantiate_slist(const char *fname)
     return head;
 }
 
-void free_slist(struct slist_stud *head) {
+void print_slist(struct slist_stud *snames)
+{
+    int i = 0;
+    struct student *el;
+    SLIST_FOREACH(el, snames, classmates)
+    {
+        printf("OUTPUT: i %d len %zu '%s' '%zu'\n", i++, el->nlen, el->name, el->hash);
+    }
+}
+
+void free_slist_items(struct slist_stud *head) {
     while (!SLIST_EMPTY(head))
     {
         struct student *s = SLIST_FIRST(head);
@@ -93,10 +104,72 @@ void free_slist(struct slist_stud *head) {
     }
 }
 
+// tree.h integration
+
+struct node {
+    RB_ENTRY(node) entry;
+    struct student *s;
+};
+
+int studcmp(struct node *n1, struct node *n2)
+{
+    return (n1->s->hash < n2->s->hash ? -1 : n1->s->hash > n2->s->hash);
+}
+
+RB_HEAD(studtree, node);
+RB_PROTOTYPE(studtree, node, entry, studcmp);
+RB_GENERATE(studtree, node, entry, studcmp);
+
+struct studtree* instantiate_tree(struct slist_stud *snames)
+{
+    struct studtree *head = malloc(sizeof(struct studtree));
+
+    if (head == NULL) return NULL;
+
+    struct node *n;
+    struct student *s;
+    SLIST_FOREACH(s, snames, classmates)
+    {
+        if ((n = malloc(sizeof(struct node))) == NULL)
+        {
+            return head;
+        }
+
+        n->s = s;
+        RB_INSERT(studtree, head, n);
+    }
+
+    return head;
+}
+
+// FIX: implement tree pretty print
+void print_tree(struct studtree *stree)
+{
+    int i = 0;
+    struct node *n;
+    RB_FOREACH(n, studtree, stree)
+    {
+        struct student *el = n->s;
+        printf("OUTPUT: i %d len %zu '%s' '%zu'\n", i++, el->nlen, el->name, el->hash);
+    }
+}
+
+void free_tree_nodes(struct studtree *stree) {
+    struct node *n, *np;
+
+    RB_FOREACH_SAFE(n, studtree, stree, np)
+    {
+        RB_REMOVE(studtree, stree, n);
+        free(n);
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    // checks if input filename is provided
     if (argc > 2) return -1;
 
+    // SLIST creation
     struct slist_stud *snames = instantiate_slist(argv[1]);
 
     if (snames == NULL) _exit(EXIT_FAILURE);
@@ -106,14 +179,30 @@ int main(int argc, char *argv[])
         _exit(EXIT_FAILURE);
     }
 
-    int i = 0;
-    struct student *el;
-    SLIST_FOREACH(el, snames, classmates)
+    // SLIST test
+    print_slist(snames);
+
+    // tree creation
+    struct studtree *stree = instantiate_tree(snames);
+
+    if (stree == NULL) {
+        free_slist_items(snames);
+        free(snames);
+        _exit(EXIT_FAILURE);
+    }
+    else if (RB_EMPTY(stree))
     {
-        printf("OUTPUT: i %d len %zu '%s' '%zu'\n", i++, el->nlen, el->name, el->hash);
+        free_slist_items(snames);
+        free(snames);
+        free(stree);
+        _exit(EXIT_FAILURE);
     }
 
-    free_slist(snames);
+    print_tree(stree);
+
+    free_slist_items(snames);
     free(snames);
+    free_tree_nodes(stree);
+    free(stree);
     return 0;
 }
